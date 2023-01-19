dcf5f50f26ead67fbb5ff7aca645373c7784b86c
Никапель <7882587@gmail.com>
2023-01-19 16:16:16 +0200

    Сайт-портфолио/ вариант

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,15 @@
+from time import asctime, sleep
+import time
+
 from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'index.html')
 
 # Create your views here.
+
+
+
+
+
+
